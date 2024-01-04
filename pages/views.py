11cd7fbc3b8c42eeea8dd795d08f58ae22e0a4b3
DILEGENT1 <di.abaki@gmail.com>
2024-01-04 21:44:27 +0400

    add templates

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request) :
-    return HttpResponse("<h1>Hello Django </h1>")
+    return render(request, 'pages/index.html')     #return HttpResponse("<h1>Hello Django </h1>")
+def about(request) :
+    return render(request, 'pages/about.html')
