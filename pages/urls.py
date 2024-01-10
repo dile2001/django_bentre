2964ccce5000eb6cd58b0944b9cd066066163538
DILEGENT1 <di.abaki@gmail.com>
2024-01-10 23:11:53 +0400

    link and if condition

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('index', views.index, name='index'),
+    path('about', views.about, name='about'),
+     path('index.html', views.index, name='index'),
+    path('about.html', views.about, name='about')
 ]
