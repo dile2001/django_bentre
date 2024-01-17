87abaf8b1ba63191ad3918daff39c0cd8c1850a5
DILEGENT1 <di.abaki@gmail.com>
2024-01-17 16:44:36 +0400

    add default home page path

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('index', views.index, name='index'),
     path('about', views.about, name='about'),
-     path('index.html', views.index, name='index'),
+    path('index.html', views.index, name='index'),
     path('about.html', views.about, name='about')
 ]
